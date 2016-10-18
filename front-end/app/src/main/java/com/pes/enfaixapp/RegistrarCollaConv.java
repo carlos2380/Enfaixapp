fc8b87993e8387a90f0dd9d83d0601c5906da768
Àlex Correa Tomàs <alex.correa@est.fib.upc.edu>
2016-10-18 17:59:01 +0200

    disseny principal login

--- front-end/app/src/main/java/com/pes/enfaixapp/RegistrarCollaConv.java
+++ front-end/app/src/main/java/com/pes/enfaixapp/RegistrarCollaConv.java
@@ -2,6 +2,8 @@ package com.pes.enfaixapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Àlex on 17/10/2016.
@@ -9,9 +11,37 @@ import android.os.Bundle;
 
 public class RegistrarCollaConv extends Activity {
 
-    @Override public void onCreate(Bundle savedInstanceState) {
+    Button button;
+    Button button2;
+    Button button3;
+    Button button4;
+
+    View lay1;
+    View lay2;
+    View lay3;
+    View lay4;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registre_escollir_collaconv);
+
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+
+        lay1 =  findViewById(R.id.uno);
+        lay2 =  findViewById(R.id.dos);
+        lay3 = findViewById(R.id.tres);
+        lay4 = findViewById(R.id.cuatro);
+
+        lay1.setVisibility(View.GONE);
+        lay2.setVisibility(View.VISIBLE);
+        lay3.setVisibility(View.GONE);
+        lay4.setVisibility(View.GONE);
+
     }
 
 
