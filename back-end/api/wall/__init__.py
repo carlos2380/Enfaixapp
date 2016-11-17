746ff7d9532003d3529dc3545898cc3e827a2d1a
Eduard Maura <edu.maura.puig@gmail.com>
2016-11-17 20:39:11 +0100

    Little  refactor

--- back-end/api/wall/__init__.py
+++ back-end/api/wall/__init__.py
@@ -3,13 +3,13 @@ from flask import jsonify
 from flask import make_response
 
 from api import app
-from api.wall.rss import rssInfo
+from api.wall.rss import rss_info
 
 
 @app.route('/wall', methods=['GET'])
 # @requires_auth
 def get_wall():
-    noticies = rssInfo("http://www.cccc.cat/continguts/rss/noticies-1")
+    noticies = rss_info("http://www.cccc.cat/continguts/rss/noticies-1")
     if noticies is None:
         abort(404)
     else:
--- back-end/api/wall/rss.py
+++ back-end/api/wall/rss.py
@@ -1,10 +1,8 @@
 import feedparser
-import json
-import codecs
 import datetime
 
 
-def rssInfo(url, month=datetime.datetime.today().month, year=datetime.datetime.today().year, number_of_entries=5):
+def rss_info(url, month=datetime.datetime.today().month, year=datetime.datetime.today().year, number_of_entries=5):
     d = feedparser.parse(url)
     json1 = {}
     id = 0
@@ -13,16 +11,13 @@ def rssInfo(url, month=datetime.datetime.today().month, year=datetime.datetime.t
         entry = {}
         date = post.published_parsed
         if date.tm_year == year and date.tm_mon == month:
-            if (count <= number_of_entries):
+            if count <= number_of_entries:
                 entry['title'] = post.title.encode('utf-8')
                 entry['link'] = post.link
                 entry['date'] = post.published
                 entry['description'] = post.description
                 json1[id] = entry
-                count = count + 1
-            id = id + 1
+                count += 1
+            id += 1
 
     return json1
-    # print id
-    # with open('data.txt', 'w') as f:
-    # json.dump(json1, f)
