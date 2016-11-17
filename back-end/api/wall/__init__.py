8e808f98e30345766194c9355a8d77af3bf97705
marc1161 <marc.marti.calabres@est.fib.upc.edu>
2016-11-17 19:36:20 +0100

    wall backend implementat el GET

--- back-end/api/wall/__init__.py
+++ back-end/api/wall/__init__.py
@@ -1,10 +1,23 @@
+from flask import abort
+from flask import json
+from flask import jsonify
+from flask import make_response
+
 from api import app
+from api.db.DB import DB
 
 
 # ask the controller to retrieve the user identified by id
 # return the resource that represents a user if found
 # otherwise, return 404 error code
+from wall.rss import rssInfo
+
+
 @app.route('/wall', methods=['GET'])
 # @requires_auth
 def get_wall():
-    pass
+    noticies = rssInfo("http://www.cccc.cat/continguts/rss/noticies-1")
+    if noticies is None:
+        abort(404)
+    else:
+        return make_response(jsonify(noticies.__dict__), 200)
