11d06ad7fecd598f9648ddeeaf14deb49d9ccb1c
Eduard Maura <edu.maura.puig@gmail.com>
2016-11-25 17:06:08 +0100

    Fix bug on User constructor method

--- back-end/api/login/auth_ctrl.py
+++ back-end/api/login/auth_ctrl.py
@@ -10,7 +10,7 @@ from api.db.CtrlFactory import get_user_ctrl, get_following_ctrl, get_belonging_
 def create_user(name, surname, email, password, belonging_list, following_list):
     db_configuration = json.loads(open("api/db/db.json").read())
     user_ctrl = get_user_ctrl(DB(db_configuration).get_database_connection())
-    user = User(name, surname, email, password)
+    user = User(name, surname, email, password=password)
     user = user_ctrl.insert(user)
     belonging_ctrl = get_belonging_ctrl(DB(db_configuration).get_database_connection())
     belonging_ctrl.insert_belonging_batch(belonging_list, user.id)
