7fe2cb157e42dd49d0ece614a64849fb456b96a8
carlos2380 <carlos.francisco.alamo@est.fib.upc.edu>
2016-12-09 22:37:00 +0100

    Lista Esdeveniments 95%

--- front-end/app/src/main/java/com/pes/enfaixapp/Adapters/EsdevenimentAdapter.java
+++ front-end/app/src/main/java/com/pes/enfaixapp/Adapters/EsdevenimentAdapter.java
@@ -1,5 +1,9 @@
 package com.pes.enfaixapp.Adapters;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,11 +11,22 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.pes.enfaixapp.Controllers.BitmapUtilities;
 import com.pes.enfaixapp.Models.Esdeveniment;
 import com.pes.enfaixapp.Models.Noticia;
 import com.pes.enfaixapp.R;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by carlos on 15/11/2016.
@@ -20,20 +35,54 @@ import java.util.List;
 public class EsdevenimentAdapter extends RecyclerView.Adapter<EsdevenimentAdapter.EsdevenimentViewHolder> {
     private List<Esdeveniment> items;
 
-    public static class EsdevenimentViewHolder extends RecyclerView.ViewHolder {
+    public static class EsdevenimentViewHolder extends RecyclerView.ViewHolder  implements OnMapReadyCallback{
         // Campos respectivos de un item
         public ImageView imagen;
         public TextView nombre;
         public TextView descripcio;
         public TextView localitzacio;
-
+        public GoogleMap mGoogleMap;
+        public Marker marker;
+        public MapView map;
+        private View viw;
         public EsdevenimentViewHolder(View v) {
 
             super(v);
-            //imagen = (ImageView) v.findViewById(R.id.imagen);
+            viw = v;
+            imagen = (ImageView) v.findViewById(R.id.fotoEsdvCard);
             nombre = (TextView) v.findViewById(R.id.tituloEsdvCard);
             descripcio = (TextView) v.findViewById(R.id.contentidoEsdvCard);
             localitzacio = (TextView) v.findViewById(R.id.localitzacioEsdvCard);
+            map = (MapView) v.findViewById(R.id.mapEsdvCard);
+
+            if (map != null)
+            {
+                map.onCreate(null);
+                map.onResume();
+                map.getMapAsync(this);
+            }
+        }
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            mGoogleMap = googleMap;
+            Geocoder geocoder = new Geocoder(viw.getContext(), Locale.getDefault());
+            List<Address> address;
+            try {
+                address = geocoder.getFromLocationName(localitzacio.getText().toString(), 1);
+                if(address.size() > 0) {
+                    LatLng latlong = new LatLng(address.get(0).getLatitude(), address.get(0).getLongitude());
+                    if(marker != null) marker.remove();
+                    //mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                    marker = mGoogleMap.addMarker(new MarkerOptions().position(latlong).title(nombre.getText().toString()));
+                    mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlong, 10.0f));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // Add a marker in Sydney and move the camera
+
         }
     }
 
@@ -45,19 +94,40 @@ public class EsdevenimentAdapter extends RecyclerView.Adapter<EsdevenimentAdapte
     public int getItemCount() {
         return items.size();
     }
-
+    View v;
     @Override
     public EsdevenimentViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View v = LayoutInflater.from(viewGroup.getContext())
+        v = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.card_esdeveniment, viewGroup, false);
         return new EsdevenimentViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(EsdevenimentViewHolder viewHolder, int i) {
-        //viewHolder.imagen.setImageResource(items.get(i).getPhotoId());
+        viewHolder.imagen.setImageBitmap(BitmapUtilities.stringToBitMap(items.get(i).getFoto()));
         viewHolder.nombre.setText(items.get(i).getTitol());
         viewHolder.descripcio.setText(items.get(i).getDescripcio());
-        viewHolder.localitzacio.setText(items.get(i).getDescripcio());
+        viewHolder.localitzacio.setText("Lloc: " + items.get(i).getDireccio());
+        GoogleMap thisMap = viewHolder.mGoogleMap;
+        if(thisMap != null) {
+            Geocoder geocoder = new Geocoder(v.getContext(), Locale.getDefault());
+            List<Address> address;
+            try {
+                address = geocoder.getFromLocationName(items.get(i).getDireccio(), 1);
+                if(address.size() > 0) {
+                    LatLng latlong = new LatLng(address.get(0).getLatitude(), address.get(0).getLongitude());
+                    Marker marker;
+                    //mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                    marker = thisMap.addMarker(new MarkerOptions().position(latlong).title(items.get(i).getTitol()));
+                    thisMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlong, 10.0f));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
+    //MAPA GOOGLE
+
 }
