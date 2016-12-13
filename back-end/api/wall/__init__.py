945d18766bf5cf8acb419e23879bfe068341982b
Eduard Maura <edu.maura.puig@gmail.com>
2016-12-13 17:26:58 +0100

    GET /wall?user_id=

--- back-end/api/wall/__init__.py
+++ back-end/api/wall/__init__.py
@@ -2,34 +2,31 @@ from flask import abort
 from flask import json
 from flask import jsonify
 from flask import make_response
+from flask import request
 
 from api import app
 from api.wall.rss import rss_info, busca_rss
 from api.db.DB import DB
-from api.db.CtrlFactory import get_follow_ctrl, get_belong_ctrl
+from api.db.CtrlFactory import get_follow_ctrl, get_belong_ctrl, get_user_ctrl
 
 
 @app.route('/wall', methods=['GET'])
-# @requires_auth
 def get_wall():
-    noticies = rss_info("http://www.cccc.cat/continguts/rss/noticies-1")
-    if noticies is None:
-        abort(404)
+    user_id = request.args.get('user_id')
+    noticies = []
+    if user_id is not None:
+        db_configuration = json.loads(open("api/db/db.json").read())
+        user_ctrl = get_user_ctrl(DB(db_configuration).get_database_connection())
+        user = user_ctrl.get(user_id)
+        if user:
+            follow_ctrl = get_follow_ctrl(DB(db_configuration).get_database_connection())
+            colles_follow = follow_ctrl.get_name_followed_colles_by_user(user_id)
+            belong_ctrl = get_belong_ctrl(DB(db_configuration).get_database_connection())
+            colles_belongs = belong_ctrl.get_name_belonging_colles_by_user(user_id)
+            colles = colles_follow + colles_belongs
+            noticies = busca_rss("http://www.cccc.cat/continguts/rss/noticies-1", colles)
+        else:
+            abort(404)
     else:
-        return make_response(jsonify(noticies), 200)
-
-
-@app.route('/wall/<int:user_id>', methods=['GET'])
-def get_wall_by_favourite(user_id):
-    dbconfiguration = json.loads(open("api/db/db.json").read())
-    follow_ctrl = get_follow_ctrl(DB(dbconfiguration).get_database_connection())
-    colles_follow = follow_ctrl.get_name_followed_colles_by_user(user_id)
-    belong_ctrl = get_belong_ctrl(DB(dbconfiguration).get_database_connection())
-    colles_belongs = belong_ctrl.get_name_belonging_colles_by_user(user_id)
-    colles = colles_follow + colles_belongs
-    if not colles:
-        abort(404)
-    noticies = busca_rss("http://www.cccc.cat/continguts/rss/noticies-1", colles)
-    if not noticies:
-        abort(404)
+        noticies = rss_info("http://www.cccc.cat/continguts/rss/noticies-1")
     return make_response(jsonify(noticies), 200)
