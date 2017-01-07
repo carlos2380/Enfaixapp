abc33195be88aab06092b12adc68801c3b2c7b07
Eduard Maura <edu.maura.puig@gmail.com>
2017-01-07 22:55:06 +0100

    Correct error when the request is not correct

--- back-end/api/practices/__init__.py
+++ back-end/api/practices/__init__.py
@@ -1,6 +1,5 @@
 import urllib
 
-import flask
 from flask import abort, jsonify
 from flask import json
 from flask import make_response
@@ -11,34 +10,41 @@ import api.db.CtrlFactory
 from api.db.DB import DB
 from api.practices.Practice import Practice
 from api.practices.Attendant import Attendant
+from api.colles import colla_service
 
 
 @app.route('/practices', methods=['GET'])
-# @requires_auth
 def get_practices():
     db_configuration = json.loads(open("api/db/db.json").read())
     practices_ctrl = api.db.CtrlFactory.get_practices_ctrl(DB(db_configuration).get_database_connection())
     colla_id = request.args.get('colla_id')
     if colla_id is None:
-        abort(404)
+        abort(400)
     else:
-        practices = practices_ctrl.get_practices(colla_id)
-        json_practices_list = json.dumps([practice.__dict__ for practice in practices], ensure_ascii=False, encoding="utf-8")
-        return make_response(json_practices_list, 200)
-
+        colla = colla_service.get_some_info_colla(colla_id)
+        if colla:
+            practices = practices_ctrl.get_practices(colla.id)
+            json_practices_list = json.dumps([practice.__dict__ for practice in practices],
+                                             ensure_ascii=False, encoding="utf-8")
+            return make_response(json_practices_list, 200)
+        else:
+            abort(404)
 
 
 @app.route('/practices/<int:id_practice>', methods=['GET'])
-# @requires_auth
 def get_attendants(id_practice):
     db_configuration = json.loads(open("api/db/db.json").read())
     practices_ctrl = api.db.CtrlFactory.get_practices_ctrl(DB(db_configuration).get_database_connection())
-    attendants = practices_ctrl.get_attendants(id_practice)
-    json_attendants_list = json.dumps([attendant for attendant in attendants], ensure_ascii=False, encoding="utf-8")
-    return make_response(json_attendants_list, 200)
+    practice = practices_ctrl.get_practice_by_id(id_practice)
+    if practice:
+        attendants = practices_ctrl.get_attendants(id_practice)
+        json_attendants_list = json.dumps([attendant for attendant in attendants], ensure_ascii=False, encoding="utf-8")
+        return make_response(json_attendants_list, 200)
+    else:
+        abort(404)
+
 
 @app.route('/practices/<int:id_practice>', methods=['DELETE'])
-# @requires_auth
 def delete_practice(id_practice):
     db_configuration = json.loads(open("api/db/db.json").read())
     practices_ctrl = api.db.CtrlFactory.get_practices_ctrl(DB(db_configuration).get_database_connection())
@@ -46,7 +52,8 @@ def delete_practice(id_practice):
         abort(404)
     else:
         practices_ctrl.delete_practice(id_practice)
-    return make_response(jsonify({}), 200)
+        return make_response(jsonify({}), 200)
+
 
 @app.route('/practices', methods=['DELETE'])
 # @requires_auth
@@ -67,10 +74,12 @@ def delete_attendants():
             practices_ctrl.delete_attendants(id_practice, id_user)
     return make_response(jsonify({}), 200)
 
+
 @app.route('/practices/<int:id_practice>', methods=['PUT'])
 # @requires_auth
 def modify_practice(id_practice):
     pass
+
 
 @app.route('/practices', methods=['POST'])
 # @requires_auth
@@ -94,9 +103,10 @@ def create_assistance(id_practice):
     id_user = body['id_user']
     db_configuration = json.loads(open("api/db/db.json").read())
     practice_ctrl = api.db.CtrlFactory.get_practices_ctrl(DB(db_configuration).get_database_connection())
-    attendant = Attendant(id_user = id_user, id_practice =id_practice)
+    attendant = Attendant(id_user=id_user, id_practice=id_practice)
     attendant = practice_ctrl.insert_attendant(attendant)
     return make_response(jsonify(attendant.__dict__), 201)
+
 
 @app.route('/practices/<int:id_practice>', methods=['PUT'])
 def update_practice(id_practice):
